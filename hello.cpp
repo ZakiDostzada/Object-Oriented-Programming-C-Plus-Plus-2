0d52727c199336fe3f5cebd9c4836138eb733c53
ZakiDostzada <zakidostzada@gmail.com>
2025-11-05 19:55:08 -0800

    Initial commit for lab 1

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*
+In this code we are learning different libraries and the uses
+*/
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
